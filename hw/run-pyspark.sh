6a687c95d19477c4d2a7c88cdf474c2753111279
Ubuntu <ubuntu@ip-10-192-11-193.us-east-2.compute.internal>
2024-05-23 01:38:57 +0000

    troubleshooting

--- hw/run-pyspark.sh
+++ hw/run-pyspark.sh
@@ -1,5 +1,6 @@
 echo "Starting Pyspark container"
-docker run -v ~/DE300/hw:/tmp/hw3 -it \
+docker run -v ~/DE300/hw/src:/tmp/hw3 \
+	   -v ~/DE300/hw/data:/tmp/data -it \
            -p 8888:8888 \
            --name spark-sql-container \
 	   pyspark-image
